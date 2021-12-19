3ffff9efac9cb5ea7801c8f8b5dd74aa204b6e44
jbschulte <96090144+jbschulte@users.noreply.github.com>
2021-12-19 18:56:17 +0100

    Add files via upload

--- R/sfb.R
+++ R/sfb.R
@@ -1,22 +1,4 @@
 # This is a small R packages that plots sectoral financial balances of countries in the AMECO data set.
-#
-#
-
-
-# Function: input dataset and country name, output sectoral balances plot
-plot_SFB <- function(data, country) {
-  ggplot2::ggplot(
-    data = data[data$Country == country,],
-    mapping = ggplot2::aes(x=period, y=balance)
-  ) +
-    ggplot2::geom_line(mapping = ggplot2::aes(color=Sector)) +
-    ggplot2::ggtitle(country)+
-    ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))+
-    ggplot2::labs(caption="Source: AMECO")+
-    ggplot2::ylab("Financial Balance in % of GDP")+
-    ggplot2::xlab("Year")
-
-}
 
 
 #' #' Sectoral Balances
@@ -35,6 +17,41 @@ sfb <- function(list_of_countries, t_start = 1950, t_end=9999, getData=F, getPlo
   # change first letter to uppercase
   list_of_countries <- stringr::str_to_title(list_of_countries)
 
+  # create AMECO ID-Codes using create_id() function
+  idcodes <- create_id(list_of_countries)
+
+  # load AMECO data for requested countries
+  df <- rdbnomics::rdb(ids = idcodes);
+
+  # clean up data using clean_data() function
+  df_wide <- clean_data(data = df, t_start = t_start, t_end = t_end)
+
+  # calculate sectoral balances to GDP using calculate_sfb() function
+  df_wide <- calculate_sfb(data = df_wide)
+
+  # select columns
+  df_wide <- df_wide[,c("period", "Country", "Households", "Corporations", "Government", "Foreign")]
+
+  # return data if argument "getData" was set TRUE
+  if(isFALSE(getPlot) & isTRUE(getData)){
+    return(df_wide)
+  }
+
+  # plot sectoral balances using plot_SFB() function
+  plots <- lapply(list_of_countries, plot_SFB, data = df_wide)
+
+  # return plot and data
+  if(isFALSE(getData)){
+    return(plots)
+  } else {
+    return(list(plots, df_wide))
+  }
+}
+
+
+# Function: create_id()
+# Create AMECO id-codes for sectoral net savings
+create_id <- function(list_of_countries){
   # converts county names into standardized country codes
   country <- countrycode::countrycode(list_of_countries,
                                       "country.name", "wb")
@@ -44,12 +61,21 @@ sfb <- function(list_of_countries, t_start = 1950, t_end=9999, getData=F, getPlo
 
   # create ids in the rdbnomics format (e. g. "AMECO/UBLH/DEU.1.0.0.0.UBLH)
   idcodes <- vector()
-  idcodes <- rep(paste0("AMECO/", sectorcodes), each = length(country))
-  idcodes <- paste0(idcodes, "/", country)
-  idcodes <- paste0(idcodes, ".1.0.0.0.", rep(sectorcodes, each = length(country)))
+  if(length(list_of_countries) == 1){
+    idcodes <- paste0("AMECO/", sectorcodes, "/", country, ".1.0.0.0.", sectorcodes)
+  } else{
+    idcodes <- rep(paste0("AMECO/", sectorcodes), each = length(country))
+    idcodes <- paste0(idcodes, "/", country)
+    idcodes <- paste0(idcodes, ".1.0.0.0.", rep(sectorcodes, each = length(country)))
+  }
 
-  # load AMECO data for requested countries
-  df <- rdbnomics::rdb(ids = idcodes);
+  return(idcodes)
+}
+
+# Function: clean_data()
+# Clean up data
+clean_data <- function(data, t_start, t_end){
+  df <- data
 
   # reshape data to wide format and delete missing values
   df_wide <- data.table::dcast(df, period+Country+geo~dataset_code, value.var="value")
@@ -60,33 +86,38 @@ sfb <- function(list_of_countries, t_start = 1950, t_end=9999, getData=F, getPlo
   t_end <- paste0(t_end, "-01-01")
   df_wide <- df_wide[df_wide$period >= t_start & df_wide$period <= t_end,]
 
-  # calculate sectoral balances to GDP
-  df_wide$Foreign <- -df_wide$UBLA/df_wide$UVGD
-  df_wide$Corporations<- df_wide$UBLC/df_wide$UVGD
-  df_wide$Households <- df_wide$UBLH/df_wide$UVGD
-  df_wide$Government <- df_wide$UBLGE/df_wide$UVGD
+  return(df_wide)
+}
 
-  # select columns
-  df_wide <- df_wide[,c("period", "Country", "Households", "Corporations", "Government", "Foreign")]
 
-  # return data if argument "getData" was set TRUE
-  if(isFALSE(getPlot) & isTRUE(getData)){
-    return(df_wide)
-  }
+# Function: calculate_sfb()
+# Calculate sectoral balances of GDP
+calculate_sfb <- function(data){
+  df_wides <- data
+  df_wide$Foreign <- -df_wide$UBLA/df_wide$UVGD*100
+  df_wide$Corporations<- df_wide$UBLC/df_wide$UVGD*100
+  df_wide$Households <- df_wide$UBLH/df_wide$UVGD*100
+  df_wide$Government <- df_wide$UBLGE/df_wide$UVGD*100
 
-  # reshape data to long format and rename
-  df_long <- data.table::melt(df_wide, id.vars=c("period", "Country"), variable.name = "Sector", value.name = "balance")
+  return(df_wide)
+}
 
-  # plot sectoral balances using plot_sectoral_balances function
-  plots <- lapply(list_of_countries, plot_SFB, data = df_long)
 
-  # return plot and data
-  if(isFALSE(getData)){
-    return(plots)
-  } else {
-    return(list(plots, df_wide))
-  }
+# Function: plot_SFB()
+# Function: input dataset and country name, output sectoral balances plot
+plot_SFB <- function(data, country) {
+  df_long <- data.table::melt(data,
+                              id.vars=c("period", "Country"),
+                              variable.name = "Sector",
+                              value.name = "balance")
 
+  ggplot2::ggplot(
+    data = df_long[df_long$Country == country,],
+    mapping = ggplot2::aes(x=period, y=balance)
+  ) +
+    ggplot2::geom_line(mapping = ggplot2::aes(color=Sector)) +
+    ggplot2::ggtitle(country)+
+    ggplot2::theme(axis.title=ggplot2::element_blank())
 }
 
 
